c3ee4807b70ae7d87a7700a3f9510ec317a61841
Francisco Soliz <francisco-soliz@hotmail.com>
2025-07-24 15:14:59 -0500

    cicd

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter> {/* ← ENVOLVER AQUÍ */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
