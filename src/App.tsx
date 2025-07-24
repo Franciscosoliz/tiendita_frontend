317e3aaccd320a89aa3c275dd87dcc81442e45d0
Francisco Soliz <francisco-soliz@hotmail.com>
2025-07-24 15:18:06 -0500

    cicd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as  Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navegacion from './components/Navbar';
 import Productos from './pages/Productos';
 import { CartProvider } from './components/CartContext'; // ajusta la ruta
@@ -9,14 +9,18 @@ import RegisterForm from './components/RegisterForm';
 function App() {
   return (
     <CartProvider>
-      <Navegacion />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Productos />} />
-        <Route path="/productos/:tipo" element={<Productos />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-      </Routes>
+      <Router>
+        <Navegacion />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/productos/:tipo" element={<Productos />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          {/* <Route path="/carrito" element={<CartProvider />} /> */}
+          {/* Otras rutas */}
+        </Routes>
+      </Router>
     </CartProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter } from 'react-router-dom';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ← ENVOLVER AQUÍ */}
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 );
