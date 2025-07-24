bd47f9e97845f2b5f03b4384fa31f8956313bf17
Francisco Soliz <francisco-soliz@hotmail.com>
2025-07-24 15:06:25 -0500

    cicd

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'https://tiendita-api.desarrollo-software.xyz';
-const API_BASE_URL = 'http://localhost:2990';
+const API_BASE_URL = 'https://nestjs-tiendita-backend-api.desarrollo-software.xyz/';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
